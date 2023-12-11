d0406134fd49ca034fe44d8112d292feb2fe1904
Praveen S <praveenss2806@gmail.com>
2023-12-11 22:31:40 +0530

    added close buttion

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
 import Card from './shared/Card'
+import {FaTimes} from 'react-icons/fa'
 
 const FeedbackItem = ({feedback}) => {
 
     return (
         <Card>
             <div className="num-display">{feedback.rating}</div>
+            <button className='close'>
+                <FaTimes color="purple"/>
+            </button>
             <div className="text-display">{feedback.text}</div>
         </Card>
     )
